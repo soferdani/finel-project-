c2768b348c2634df869b1d7f4a7671c26bd46025
Idan Shalem <idanshalem25@gmail.com>
2020-10-28 19:27:09 +0200

    added routs component and home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Button, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Link, Redirect, Route, useHistory } from 'react-router-dom'
 import { Auth } from "aws-amplify"
-// import NavBar from './components/Layout/NavBar'
 import './App.css';
 import Login from './components/auth/Login';
 import Container from './components/Layout/Container';
 import Signup from './components/auth/Signup';
 import { inject, observer } from 'mobx-react';
+import Routes from './components/Routes';
 
 const App = inject('user')(observer((props) => {
 
@@ -33,44 +33,11 @@ const App = inject('user')(observer((props) => {
     await setIsAuthenticating(false)
   }
 
-  const handleLogout = async () => {
-    await Auth.signOut()
-    user.userHasAuthenticated(false)
-    return <Redirect to='/login' />
-  }
+  
 
   return (
     <Grid >
-      <Router>
-        {user.isAuthenticated === true
-          ?   <Redirect from='/' to='/home' />
-          :   <Redirect from='/' to='/login' />}
-        <Route 
-          path='/login' 
-          exact render={({ match }) => 
-            <Login 
-              match={match} 
-            />
-          }
-        />
-        <Route 
-          path='/signup' 
-          exact render={({ match }) => 
-            <Signup 
-              match={match} 
-            />
-          }
-        />
-        <Route 
-            path='/home' 
-            exact render={({ match }) => 
-              <Container 
-                handleLogout={handleLogout}
-                match={match} 
-              />
-            }
-          />
-      </Router>
+      <Routes />
     </Grid>
   )
 }))
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { Grid } from '@material-ui/core'
+import { inject, observer } from 'mobx-react'
+import React from 'react'
+
+const Home = inject('user')(observer((props) => {
+
+    return (
+        <Grid>
+
+        </Grid>
+    )
+
+}))
+
+export default Home
