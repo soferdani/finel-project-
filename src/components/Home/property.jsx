7a163c4510f5347fa6c61715253dfaed1ae3526a
Idan Shalem <idanshalem25@gmail.com>
2020-10-29 12:56:50 +0200

    changed menu layout and added avatar

---
+++ src/components/Home/property.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { 
+    Card, 
+    CardActionArea, 
+    CardActions, 
+    CardContent, 
+    CardMedia, 
+    Button,
+    Typography 
+} from '@material-ui/core'
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+})
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
