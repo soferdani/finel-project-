1caecee6dac15b46c66c869f63ec41a3bd044aaf
soferdani <soferdani@gmail.com>
2020-10-29 16:25:04 +0200

    dockerImg

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -3,92 +3,92 @@ import axios from 'axios'
 const UserService = function () {
 
     const getUserDetails = async function (email) {
-        const user = await axios.get(`http://localhost:3001/user/${email}`)
+        const user = await axios.get(`/user/${email}`)
         return user.data
     }
 
     const getUserProperties = async (id) => {
-        const userProperties = await axios.get(`http://localhost:3001/properties/${id}`)
+        const userProperties = await axios.get(`/properties/${id}`)
         return userProperties.data
     }
 
     const getPropertyTodo = async (id) => {
-        const todoList = await axios.get(`http://localhost:3001/todos/${id}`)
+        const todoList = await axios.get(`/todos/${id}`)
         return todoList.data
     }
 
     const getServiceWorkers = async (id) => {
-        const serviceWorkers = await axios.get(`http://localhost:3001/service/${id}`)
+        const serviceWorkers = await axios.get(`/service/${id}`)
         return serviceWorkers.data
     }
 
     const getBooking = async (id) => {
-        const booking = await axios.get(`http://localhost:3001/booking-all/${id}`)
+        const booking = await axios.get(`/booking-all/${id}`)
         return booking.data
     }
 
     const addNewProperty = async (propertyDetails) => {
-        const newProperty = await axios.post(`http://localhost:3001/property`, propertyDetails)
+        const newProperty = await axios.post(`/property`, propertyDetails)
         return newProperty.data
     }
 
     const addNewTodo = async (todo) => {
-        const newTodo = await axios.post('http://localhost:3001/property', todo)
+        const newTodo = await axios.post('/property', todo)
         return newTodo.data
     }
 
     const addNewServiceWorker = async (serviceWorker) => {
-        const newServiceWorker = await axios.post('http://localhost:3001/service-create', serviceWorker)
+        const newServiceWorker = await axios.post('/service-create', serviceWorker)
         return newServiceWorker.data
     }
 
     const addNewBooking = async (booking) => {
-        const id = await axios.post('http://localhost:3001/booking', booking)
+        const id = await axios.post('/booking', booking)
         return id.data
     }
 
     const updateUserDetails = async (userId, userNewDetails) => {
-        const userDetails = await axios.put(`http://localhost:3001/user/${userId}`, userNewDetails)
+        const userDetails = await axios.put(`/user/${userId}`, userNewDetails)
         return userDetails.data
     }
 
     const updateProperty = async (propertyId, updateDetails) => {
-        const updatedProperty = await axios.put(`http://localhost:3001/property/${propertyId}`, updateDetails)
+        const updatedProperty = await axios.put(`/property/${propertyId}`, updateDetails)
         return updatedProperty.data
     }
 
     const updateTodoDetails = async (bookingId, bookingDetails) => {
-        const booking = await axios.put(`http://localhost:3001/booking/${bookingId}`, bookingDetails)
+        const booking = await axios.put(`/booking/${bookingId}`, bookingDetails)
         return booking.data
     }
 
     const updateBookingDetails = async (todoId, todoDetails) => {
-        const todo = await axios.put(`http://localhost:3001/todo/${todoId}`, todoDetails)
+        const todo = await axios.put(`/todo/${todoId}`, todoDetails)
         return todo.data
     }
 
     const updateTodoStatus = async (todoId, status) => {
-        const todo = await axios.put(`http://localhost:3001/todo/${todoId}`, status )
+        const todo = await axios.put(`/todo/${todoId}`, status )
         return todo.data
     }
 
     const deleteProperty = async (propertyId) => {
-        const property = await axios.delete(`http://localhost:3001/property/${propertyId}`)
+        const property = await axios.delete(`/property/${propertyId}`)
         return property.data
     }
 
     const deleteTodo = async (todoId) => {
-        const todo = await axios.delete(`http://localhost:3001/todo/${todoId}`)
+        const todo = await axios.delete(`/todo/${todoId}`)
         return todo.data
     }
 
     const deleteServiceWorkers = async (propertyId, ServiceWorkerId) => {
-        const deleted = await axios.delete(`http://localhost:3001/service/${propertyId}/${ServiceWorkerId}`)
+        const deleted = await axios.delete(`/service/${propertyId}/${ServiceWorkerId}`)
         return deleted.data
     }
 
     const deleteBooking = async (bookingId) => {
-        const deleted = await axios.delete(`http://localhost:3001/booking-delete/${bookingId}`)
+        const deleted = await axios.delete(`/booking-delete/${bookingId}`)
         return deleted.data
     }
 
