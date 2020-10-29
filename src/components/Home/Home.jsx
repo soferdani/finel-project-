c9a3aa9a2ceb3ebcc2d49751d1031472cdda9f84
Idan Shalem <idanshalem25@gmail.com>
2020-10-29 13:03:44 +0200

    starting to work on properties cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,24 @@
-import { Grid } from '@material-ui/core'
+import { Grid, makeStyles } from '@material-ui/core'
 import { inject, observer } from 'mobx-react'
 import React from 'react'
+import Property from './property'
+
+const useStyles = makeStyles({
+    homeContainer: {
+        marginLeft: 200,
+        padding: '40px'
+    }
+})
 
 const Home = inject('user')(observer((props) => {
 
     const { user } = props
-    
+
+    const classes = useStyles()
+
     return (
-        <Grid>
-            
+        <Grid className={classes.homeContainer}>
+            <Property />
         </Grid>
     )
 
--- src/components/Home/property.jsx
+++ src/components/Home/property.jsx
@@ -9,6 +9,7 @@ import {
     Button,
     Typography 
 } from '@material-ui/core'
+import { inject, observer } from 'mobx-react'
 
 
 const useStyles = makeStyles({
@@ -17,37 +18,40 @@ const useStyles = makeStyles({
   },
 })
 
-export default function ImgMediaCard() {
-  const classes = useStyles();
+const Property = inject('user')(observer((props) => {  
+    
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  )
-}
+    return (
+        <Card className={classes.root}>
+        <CardActionArea>
+            <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary">
+            Share
+            </Button>
+            <Button size="small" color="primary">
+            Learn More
+            </Button>
+        </CardActions>
+        </Card>
+    )
+}))
+
+export default Property
